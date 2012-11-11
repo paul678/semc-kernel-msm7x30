afd3d3bd7151b3c9ea892730b1f7f6163c090ce3
szl.kiev <szl.kiev@gmail.com>
2012-11-11 13:00:43 +0700

    Revert "define MSMFB_OVERLAY_COMMIT"
    Change-Id: I48684807eb64d60c3e99e5aa5af2033a5ee7b1b8

--- include/linux/msm_mdp.h
+++ include/linux/msm_mdp.h
@@ -47,7 +47,6 @@
 						struct msmfb_mixer_info_req)
 #define MSMFB_OVERLAY_PLAY_WAIT _IOWR(MSMFB_IOCTL_MAGIC, 149, \
 						struct msmfb_overlay_data)
-#define MSMFB_OVERLAY_COMMIT  _IOW(MSMFB_IOCTL_MAGIC, 163, unsigned int)
 
 #define MDP_IMGTYPE2_START 0x10000
 
